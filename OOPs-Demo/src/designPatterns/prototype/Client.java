ad0498f0f5aeeca092289a83f20cac48064d9d19
sndpoffcl <sandeep2official@gmail.com>
2023-01-03 09:16:43 +0530

    Prototype and Register

--- OOPs-Demo/src/designPatterns/prototype/Client.java
+++ OOPs-Demo/src/designPatterns/prototype/Client.java
@@ -14,6 +14,12 @@ public class Client {
         mayBatch.setYearOfEnrollment(2022);
         studentRegistry.register(mayBatch);
 
+        Student janBatch = new Student();
+        janBatch.setAvgBatchPsp(100);
+        janBatch.setBatchName("Jan-LLD");
+        janBatch.setYearOfEnrollment(2023);
+        studentRegistry.register(janBatch);
+
         IntelligentStudent intelligentAprBatch = new IntelligentStudent();
         intelligentAprBatch.setAvgBatchPsp(80);
         intelligentAprBatch.setBatchName("Intelligent-Apr-LLD");
@@ -28,7 +34,10 @@ public class Client {
         StudentRegistry studentRegistry = new StudentRegistry();
         studentRegistry = fillUpRegister(studentRegistry);
 
-        Student aprBatchTemplate = studentRegistry.get("Apr-LLD");
-        Student mayBatchTemplate = studentRegistry.get("May-LLD");
+        Student aprBatchStudent = studentRegistry.get("Apr-LLD").clone();
+        aprBatchStudent.setId(1);
+        aprBatchStudent.setName("Bhanu");
+        aprBatchStudent.setAge(23);
+        Student mayBatchStudent = studentRegistry.get("May-LLD").clone();
     }
 }
